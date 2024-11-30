ad8f8af109299fdb8dbfe2967598c47323a81d41
menta <brentano.l@gmail.com>
2024-11-30 14:26:28 -0300

    entity picker updates

---
+++ scripts/entity_picker.gd
@@ -0,0 +1,4 @@
+extends PanelContainer
+
+@export var target_entity: Resource
+@export var is_on_remove_state: bool = false
---
+++ scripts/entity_picker_card.gd
@@ -0,0 +1,33 @@
+extends PanelContainer
+
+@onready var button_insert: Button = %ButtonInsert
+@onready var button_remove: Button = %ButtonRemove
+@onready var entity_line: HBoxContainer = %EntityLine
+
+@export var is_remove_mode: bool = false:
+	set(new_value):
+		is_remove_mode = new_value
+		_set_mode()
+
+@export var entity: Resource:
+	set(new_entity):
+		entity = new_entity
+		if entity_line != null:
+			entity_line.data = new_entity
+
+
+func _ready() -> void:
+	_set_mode.call_deferred()
+	entity_line.data = entity
+
+
+func _set_mode() -> void:
+	if button_insert == null or button_remove == null:
+		return
+
+	if is_remove_mode == true:
+		button_remove.show()
+		button_insert.hide()
+	else:
+		button_remove.hide()
+		button_insert.show()
